1241ea89a8fac14dcd8f708cf2c1f0ce1d218086
itxmuhammadwaqarali <itxmewaqar@gmail.com>
2026-03-26 13:28:18 +0500

    update model for todo add ENUM for priority

--- app/models/todo.py
+++ app/models/todo.py
@@ -1,8 +1,13 @@
-import uuid
-from sqlalchemy import Column, String, ForeignKey, Boolean
+import uuid, enum
+from sqlalchemy import Column, String, ForeignKey, Boolean, Enum
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from app.database import Base
+
+class PriorityEnum(str, enum.Enum):
+    low = "low"
+    medium = "medium"
+    high = "high"
 
 class Todo(Base):
     __tablename__ = "todos"
@@ -11,6 +16,7 @@ class Todo(Base):
     title = Column(String)
     task = Column(String, default="")
     completed = Column(Boolean, default=False)
+    priority = Column(Enum(PriorityEnum), default=PriorityEnum.medium)
     owner_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="todos")
