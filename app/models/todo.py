90817c199d2da6dd8162553ab83ac5476cbb73c1
itxmuhammadwaqarali <itxmewaqar@gmail.com>
2026-03-26 15:34:16 +0500

    add created_at and due_date fields in todo model

--- app/models/todo.py
+++ app/models/todo.py
@@ -1,8 +1,9 @@
 import uuid, enum
-from sqlalchemy import Column, String, ForeignKey, Boolean, Enum
+from sqlalchemy import Column, String, ForeignKey, Boolean, Enum, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from app.database import Base
+from datetime import datetime
 
 class PriorityEnum(str, enum.Enum):
     low = "low"
@@ -17,6 +18,8 @@ class Todo(Base):
     task = Column(String, default="")
     completed = Column(Boolean, default=False)
     priority = Column(Enum(PriorityEnum), default=PriorityEnum.medium)
+    created_at = Column(DateTime, default=datetime.now())
+    due_date = Column(DateTime, nullable=True)
     owner_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="todos")
